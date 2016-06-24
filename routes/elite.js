246eae17316ee51952fa9c292c92c77e8ada433c
ee7klt <kltah1@gmail.com>
2016-06-24 14:35:13 +0800

    file upload logic

--- routes/elite.js
+++ routes/elite.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Profile = mongoose.model('profile');
+var fs = require('fs');
+var path = require('path');
+var uid = require('uid2');
 
+var TARGET_PATH = path.resolve(__dirname, '../writable/');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -22,6 +26,42 @@ router.post('/', function(req, res) {
     console.log('profile data' + data);
   //  res.redirect('elite')
   })
+
+  /* handle file upload */
+
+  var is;
+  var os;
+  var targetPath;
+  var targetName;
+  var tempPath = req.files.resume.path;
+  var extension = req.files.resume.path.split(/[. ]+/).pop();
+
+  // new file name
+  targetName = uid(22) + '.' + extension;
+  // store in writable directory
+  targetPath = path.join(TARGET_PATH, targetName);
+  console.log('saving file to ' + targetPath);
+
+  // read stream
+  is = fs.createReadStream(tempPath);
+  // write stream
+  os = fs.createWriteStream(targetPath);
+
+  is.pipe(os);
+  //if we are done moving the file
+  is.on('end', function() {
+
+    //delete file from temp folder
+    fs.unlink(tempPath, function(err) {
+      if (err) {
+        return res.send(500, 'Something went wrong');
+      }
+
+      console.log('done saving file');
+
+    });//#end - unlink
+  });//#end - on.end
+
 
 });
 
