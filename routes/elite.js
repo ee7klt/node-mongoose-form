0a5db345e38b7e0f528f5053e1f5658143b8009c
ee7klt <kltah1@gmail.com>
2016-06-24 19:48:14 +0800

    successful file upload using multer
    simple middleware addition to the app.post arguments

--- routes/elite.js
+++ routes/elite.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Profile = mongoose.model('profile');
-var fs = require('fs');
+var multer = require('multer');
 var path = require('path');
-var uid = require('uid2');
+
 
 var TARGET_PATH = path.resolve(__dirname, '../writable/');
 
@@ -14,7 +14,8 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/', function(req, res) {
+router.post('/',  multer({ dest: './uploads/'}).single('resume'),
+ function(req, res) {
   new Profile({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -26,41 +27,9 @@ router.post('/', function(req, res) {
     console.log('profile data' + data);
   //  res.redirect('elite')
   })
-
-  /* handle file upload */
-
-  var is;
-  var os;
-  var targetPath;
-  var targetName;
-  var tempPath = req.files.resume.path;
-  var extension = req.files.resume.path.split(/[. ]+/).pop();
-
-  // new file name
-  targetName = uid(22) + '.' + extension;
-  // store in writable directory
-  targetPath = path.join(TARGET_PATH, targetName);
-  console.log('saving file to ' + targetPath);
-
-  // read stream
-  is = fs.createReadStream(tempPath);
-  // write stream
-  os = fs.createWriteStream(targetPath);
-
-  is.pipe(os);
-  //if we are done moving the file
-  is.on('end', function() {
-
-    //delete file from temp folder
-    fs.unlink(tempPath, function(err) {
-      if (err) {
-        return res.send(500, 'Something went wrong');
-      }
-
-      console.log('done saving file');
-
-    });//#end - unlink
-  });//#end - on.end
+  console.log(req.body);
+  console.log(req.files);
+  res.status(204).end();
 
 
 });
